705e0aa6a7ae8e272dca30f683d0d462e0c5e84b
Tanay Gupta <tanaygupta.gupta@gmail.com>
2018-08-24 21:55:41 +0530

    DownloadData.java
    This code is used to download data from the internet and write it to another fie.

---
+++ DownloadData.java
@@ -0,0 +1,42 @@
+import java.net.*;
+
+
+public class DownloadData
+{
+	public static void main(String[] args)
+	{
+		try{
+		String url="Pass the url of the file to be donwloaded here";
+		
+		URL u= new URL(url);
+		
+		HttpURLConnection con=  (HttpURLConnection) u.openConnection; // Opens the connection
+		
+		InputStream is= con.getInputStream(); // It fetches the Input Stream for the connection for download
+		
+		FileOutputStream fout= new FileOutputStream("path of the output file");
+		//Path of the file where you want the downloaded data to be written
+		
+		int data= is.read();
+		while(data != -1)
+		{
+			fout.write(data);
+			data=is.read();
+		}
+		fout.close();
+		System.out.println("Write Success");
+		}catch(MalformedURLException ex)
+		{
+			
+			System.out.println(ex);
+		}catch(IOException ex)
+		{
+			System.out.println(ex);
+		}
+	
+	}
+
+
+
+
+}
